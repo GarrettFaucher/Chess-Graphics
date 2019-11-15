98f0e1840a42a7531bd6343b86df7f36004ec90b
jillianneff <jilliannneff@gmail.com>
2019-11-15 14:33:08 -0500

    Graphics Class added

---
+++ graphics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jillian Neff on 11/15/19.
+//
+
+#include "graphics.h"
---
+++ graphics.h
@@ -0,0 +1,29 @@
+//
+// Created by Jillian Neff on 11/15/19.
+//
+
+#ifndef FINALPROJECTMAJOR_GF_JN_GRAPHICS_H
+#define FINALPROJECTMAJOR_GF_JN_GRAPHICS_H
+
+
+#include <stdlib.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <iostream>
+
+
+class graphics {
+
+};
+
+
+#endif //FINALPROJECTMAJOR_GF_JN_GRAPHICS_H
